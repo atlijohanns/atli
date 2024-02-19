d090db1f1ff0b4dfe836252299b6301c828a09d1
Atli Johannsson <atlijohanns@gmail.com>
2024-02-19 00:43:21 +0000

    refactor: anchors to Links; link to GitHub project instead of copyright and the relevant translations

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
           {/* Email */}
           <span>
-            <a href="mailto:atlijohanns@gmail.com">
+            <Link href="mailto:atlijohanns@gmail.com">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -30,12 +30,12 @@ export default function Home() {
                 <rect width="20" height="16" x="2" y="4" rx="2" />
                 <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
               </svg>
-            </a>
+            </Link>
           </span>
 
           {/* LinkedIn */}
           <span>
-            <a
+            <Link
               href="https://www.linkedin.com/in/atlijohanns"
               target="_blank"
               rel="noopener noreferrer"
@@ -56,12 +56,12 @@ export default function Home() {
                 <rect width="4" height="12" x="2" y="9" />
                 <circle cx="4" cy="4" r="2" />
               </svg>
-            </a>
+            </Link>
           </span>
 
           {/* Facebook */}
           <span>
-            <a
+            <Link
               href="https://www.facebook.com/atlijohanns"
               target="_blank"
               rel="noopener noreferrer"
@@ -80,12 +80,12 @@ export default function Home() {
               >
                 <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
               </svg>
-            </a>
+            </Link>
           </span>
 
           {/* GitHub */}
           <span>
-            <a
+            <Link
               href="https://github.com/atlijohanns"
               target="_blank"
               rel="noopener noreferrer"
@@ -105,7 +105,7 @@ export default function Home() {
                 <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
                 <path d="M9 18c-4.51 2-5-2-7-2" />
               </svg>
-            </a>
+            </Link>
           </span>
         </div>
         <h1 className="text-4xl font-bold mt-10">{t("title")}</h1>
@@ -160,7 +160,17 @@ export default function Home() {
       <hr className="my-12" />
 
       <footer className="flex justify-between text-gray-600">
-        <p className="text-xs">2024 © Atli Þór Jóhannsson</p>
+        <p className="text-xs">
+          {t("viewOn")}{" "}
+          <Link
+            href="https://github.com/atlijohanns/atli"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            GitHub
+          </Link>
+        </p>
 
         {/* Accessibility */}
         <div className="flex space-x-4">
